09c48862b948813ad2a66911f2d8f50e1d77ac0b
hessyea <32811265+hessyea@users.noreply.github.com>
2024-02-04 13:33:58 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 const webpush = require('web-push');
 
 // VAPID keys should be generated only once.
-const vapidKeys = webpush.generateVAPIDKeys();
-
+if (process.env.RUN_TIMES == 0){
+  var vapidKeys = webpush.generateVAPIDKeys();
+  process.env.PUB_KEY = vapidKeys.publicKey;
+  process.env.PRIV_KEY = vapidKeys.privateKey;
+  process.env.RUN_TIMES = 1;
+  
+}
 
 webpush.setVapidDetails(
   'mailto:hasya101@gmail.com',
-  vapidKeys.publicKey,
-  vapidKeys.privateKey
+  process.env.PUB_KEY,
+  process.env.PRIV_KEY
 );
