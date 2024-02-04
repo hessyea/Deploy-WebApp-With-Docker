0639901eafe6423a4cda65ab0acf6880b6dc33a1
hessyea <32811265+hessyea@users.noreply.github.com>
2024-02-04 14:38:57 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,18 @@ webpush.setVapidDetails(
   process.env.PUB_KEY,
   process.env.PRIV_KEY
 );
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { subscription, dataToSend } = req.body;
+    return webpush
+      .sendNotification(subscription, JSON.stringify(dataToSend))
+      .then(() => {
+        return res.status(200).json({ message: "Notification sent!" });
+      })
+      .catch((err) => {
+        return res.status(400).json({ error: err });
+      });
+  }
+
+  return res.status(401).json({ message: "Method not allowed" });
+}
